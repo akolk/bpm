8e91bc4e17136657b914bf27068a338bf94d6a27
Anjo Kolk <anjo.kolk@gmail.com>
2025-02-23 10:46:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ client = openai.OpenAI()
 
 def generate_bpmn(st, text):
     response = client.chat.completions.create(
-        model="gpt-4",
+        model="gpt-4-turbo",
         response_format= { "type": "json_object" },
         messages=[{"role": "user", "content": 
                    f"""
