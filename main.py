0df1437fe8156d9997855a5acf70295ec85d0a39
Anjo Kolk <anjo.kolk@gmail.com>
2025-02-23 11:29:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ if input_type == "Text":
             st.components.v1.html(dia_code, height=550)
             for file_name, file_content in files.items():
                 st.download_button(file_name, file_content, file_name, "text/xml")
-            components.html(dia_code, height=550)
+            #st.components.v1.html(dia_code, height=550)
         else:
             st.warning("Geef een beschrijving van het proces.")
 
